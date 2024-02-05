4ee8b92aaf2868cbfcd49104e5e73923949f2e30
prakritipriya <112262065+prakritipriya@users.noreply.github.com>
2024-02-05 18:29:23 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,44 @@
+# provider "aws" {
+#     region="us-east-1"
+#     access_key = "AKIAXYKJXBVJQBSN73QY"
+#     secret_key = "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"
+# }
+
+resource "aws_instance" "web" {
+  provider = aws.us-west-2 
+  ami           = data.aws_ami.app_ami.image_id
+  instance_type = "t2.micro"
+  subnet_id = ["subnet-096b141240617b9b8","subnet-0570ef81fe6aea3ae","subnet-0e0427fcb1a51b17f"]
+  security_groups=["sg-084371fe3d9b5e452"]
+  
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+# resource "aws_vpc" "main" {
+#   cidr_block       = "10.0.0.0/16"
+#   instance_tenancy = "default"
+
+#   tags = {
+#     Name = "main"
+#   }
+# }
+
+//datasource : to call an ec2 instance
+
+data "aws_ami" "app_ami" {
+#   instance_alias = "foo" //instance id
+most_recent = true
+filter {
+  name="name"
+  values = [ "ami-0cf10cdf9fcd62d37" ]
+}
+}
+
+# resource "aws_api_gateway_domain_name" "example" {
+#   certificate_arn = aws_acm_certificate_validation.example.certificate_arn
+#   domain_name     = "api.example.com"
+# }
+
