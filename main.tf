0aa9a462cb2d0d7b338f350e257be14b49e87504
prakritipriya <112262065+prakritipriya@users.noreply.github.com>
2024-02-05 19:15:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "web" {
    ami= data.aws_ami.app_ami.image_id
 
   
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   subnet_id = "subnet-096b141240617b9b8"
   security_groups=["sg-084371fe3d9b5e452"]
   
@@ -28,28 +28,6 @@ data "aws_ami" "app_ami" {
 }
 
 
-# resource "aws_vpc" "main" {
-#   cidr_block       = "10.0.0.0/16"
-#   instance_tenancy = "default"
 
-#   tags = {
-#     Name = "main"
-#   }
-# }
 
-//datasource : to call an ec2 instance
-
-/*data "aws_ami" "app_ami" {
-#   instance_alias = "foo" //instance id
-most_recent = true
-filter {
-  name="name"
-  values = [ "ami-0cf10cdf9fcd62d37" ]
-}
-}*/
-
-# resource "aws_api_gateway_domain_name" "example" {
-#   certificate_arn = aws_acm_certificate_validation.example.certificate_arn
-#   domain_name     = "api.example.com"
-# }
 
