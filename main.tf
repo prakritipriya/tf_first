d129db11778ce46dd4a6f0e4766919f97ca0b138
prakritipriya <112262065+prakritipriya@users.noreply.github.com>
2024-02-05 19:16:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,14 +4,11 @@
  }*/
 
 resource "aws_instance" "web" {
- 
-   ami= data.aws_ami.app_ami.image_id
-
-  
+  ami           = data.aws_ami.app_ami.image_id
+  #ami           = "ami-0a3c3a20c09d6f377"
   instance_type = "t3.micro"
-  subnet_id = "subnet-096b141240617b9b8"
-  security_groups=["sg-084371fe3d9b5e452"]
-  
+  subnet_id     = "subnet-096b141240617b9b8"
+  security_groups = ["sg-084371fe3d9b5e452"]
   tags = {
     Name = "HelloWorld"
   }
